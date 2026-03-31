d82d93686d7f63ea29a6b6e6e71c8895d93996b6
Forhad Khan <forhadkhan@tuta.io>
2026-03-31 19:12:27 +0600

    build: compile assets for pro custom field migration

--- assets/js/productbay-pro-admin.asset.php
+++ assets/js/productbay-pro-admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-dom-ready', 'wp-i18n'), 'version' => '9b0246e99f40c1e0ac99');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-dom-ready', 'wp-i18n'), 'version' => '2fd369e43cd8fc7658c2');
