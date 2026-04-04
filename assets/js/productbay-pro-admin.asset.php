e84bd05c94e524925ef2db84c034abece614e2d4
Forhad Khan <forhadkhan@tuta.io>
2026-04-04 21:44:36 +0600

    build: update admin build artifacts

--- assets/js/productbay-pro-admin.asset.php
+++ assets/js/productbay-pro-admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-dom-ready', 'wp-i18n'), 'version' => 'd4cec28fc992c312cffb');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-dom-ready', 'wp-i18n'), 'version' => 'a1688697382a5cdd8bc8');
