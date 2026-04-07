9de221776f3599f5cf75bd0b4f4b3fc4c7b217e9
Forhad Khan <forhadkhan@tuta.io>
2026-04-07 16:54:44 +0600

    chore: rebuild assets

--- assets/js/productbay-pro-admin.asset.php
+++ assets/js/productbay-pro-admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-dom-ready', 'wp-i18n'), 'version' => 'e010d49742cca792c260');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-dom-ready', 'wp-i18n'), 'version' => 'eb8a9eb2b280e6f4ef64');
