6ce8fa8a55ecf0e9087d0ea4b41a3ba935f4e0d4
Forhad Khan <forhadkhan@tuta.io>
2026-04-12 10:46:09 +0600

    build: update admin assets

--- assets/js/productbay-pro-admin.asset.php
+++ assets/js/productbay-pro-admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-dom-ready', 'wp-i18n'), 'version' => '4a35c92bad9635b5ff32');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-dom-ready', 'wp-i18n'), 'version' => '5f890c1b107262ad707f');
